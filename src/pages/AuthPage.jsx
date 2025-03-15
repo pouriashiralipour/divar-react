9c032dd602098b90580ab604ad152625e9377c12
pouriashiralipour <pouriashiralipour@gmail.com>
2025-03-15 20:25:38 +0330

    add prosp to forms

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 import CheckOtpForm from "src/components/CheckOtpForm";
 import SendOtpForm from "src/components/SendOtpForm";
 
 const AuthPage = () => {
+  const [step, setStep] = useState(1);
+  const [mobile, setMobile] = useState("");
+  const [code, setCode] = useState("");
+
   return (
     <div>
-      <SendOtpForm />
-      <CheckOtpForm />
+      {step === 1 && (
+        <SendOtpForm setStep={setStep} setMobile={setMobile} mobile={mobile} />
+      )}
+      {step === 2 && (
+        <CheckOtpForm
+          setStep={setStep}
+          mobile={mobile}
+          code={code}
+          setCode={setCode}
+        />
+      )}
     </div>
   );
 };
