7bdc96c2551322467d2d56aa2c49ff3ce6195c3f
pouriashiralipour <pouriashiralipour@gmail.com>
2025-03-15 21:52:00 +0330

    create routes for pages

---
+++ src/router/Router.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router";
+import PageNotFound from "src/pages/404";
+import AdminPage from "src/pages/AdminPage";
+import AuthPage from "src/pages/AuthPage";
+import DashboardPage from "src/pages/DashboardPage";
+import HomePage from "src/pages/HomePage";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path='/dashboard' element={<DashboardPage />} />
+      <Route path='/auth' element={<AuthPage />} />
+      <Route path='/admin' element={<AdminPage />} />
+      <Route path='*' element={<PageNotFound />} />
+    </Routes>
+  );
+};
+
+export default Router;
