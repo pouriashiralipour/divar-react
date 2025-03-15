fae7440e55d6e22dc00bb412e2d9d1dfeea13034
pouriashiralipour <pouriashiralipour@gmail.com>
2025-03-15 20:21:53 +0330

    add tags for send number form

--- src/components/SendOtpForm.jsx
+++ src/components/SendOtpForm.jsx
@@ -1,5 +1,20 @@
-const SendOtpForm = () => {
-  return <div>SendOtpForm</div>;
+const SendOtpForm = ({ mobile, setMobile, setStep }) => {
+  return (
+    <form>
+      <p>ورود به حساب کاربری</p>
+      <span>
+        برای استفاده از امکانات دیوار لطفاً شماره موبایل خود را وارد کنید. کد
+        تایید به این شماره پیامک خواهد شد.
+      </span>
+      <label htmlFor='input'>شماره موبایل خود را وارد کنید</label>
+      <input
+        type='text'
+        id='input'
+        value={mobile}
+        onChange={(event) => setMobile(event.target.value)}
+      />
+    </form>
+  );
 };
 
 export default SendOtpForm;
