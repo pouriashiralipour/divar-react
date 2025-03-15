89fa23604f8af85eef5f118391937fc2b9aa2355
pouriashiralipour <pouriashiralipour@gmail.com>
2025-03-15 20:30:58 +0330

    add submit form

--- src/components/SendOtpForm.jsx
+++ src/components/SendOtpForm.jsx
@@ -1,18 +1,25 @@
 const SendOtpForm = ({ mobile, setMobile, setStep }) => {
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (mobile.length !== 11) return;
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <p>ورود به حساب کاربری</p>
       <span>
         برای استفاده از امکانات دیوار لطفاً شماره موبایل خود را وارد کنید. کد
         تایید به این شماره پیامک خواهد شد.
       </span>
-      <label htmlFor='input'>شماره موبایل خود را وارد کنید</label>
+      <label htmlFor='input'>شماره موبایل خود را وارد کنید .</label>
       <input
         type='text'
         id='input'
         value={mobile}
         onChange={(event) => setMobile(event.target.value)}
       />
+      <button type='submit'>ارسال کد تایید</button>
     </form>
   );
 };
