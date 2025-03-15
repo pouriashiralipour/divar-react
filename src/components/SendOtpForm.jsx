ace245551afc83d09b8d6459fb0f4ea9eb338836
pouriashiralipour <pouriashiralipour@gmail.com>
2025-03-15 20:39:12 +0330

    adding sendOtp to submit handler for sending data to db

--- src/components/SendOtpForm.jsx
+++ src/components/SendOtpForm.jsx
@@ -1,8 +1,16 @@
+import { sendOtp } from "src/services/auth";
+
 const SendOtpForm = ({ mobile, setMobile, setStep }) => {
-  const submitHandler = (event) => {
+  const submitHandler = async (event) => {
     event.preventDefault();
 
     if (mobile.length !== 11) return;
+
+    const { response, error } = await sendOtp(mobile);
+
+    if (response) setStep(2);
+
+    if (error) console.log(error.response.data.message);
   };
 
   return (
