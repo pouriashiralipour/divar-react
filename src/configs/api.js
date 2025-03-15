b254f0316f4424d5e3a571b5f43178c8e49cbfb4
pouriashiralipour <pouriashiralipour@gmail.com>
2025-03-15 22:51:47 +0330

    add accessToken to header

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getCookie } from "src/utils/cookie";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
@@ -6,5 +7,18 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+api.interceptors.request.use(
+  (request) => {
+    const accessToken = getCookie("accessToken");
+    if (accessToken) {
+      request.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default api;
